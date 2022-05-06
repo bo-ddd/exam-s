77966721d9acb60a3a89c9ee6e4130d25ae6bc4e
bigC <814151789@qq.com>
2022-05-06 10:17:52 +0800

    update code

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ module.exports = app =>{
         }
     })
     app.validator.addRule('phone',(rule,value)=>{
-        let reg = /^((13[0-9])|(17[0-1,6-8])|(15[^4,\\D])|(18[0-9]))\d{8}$/;
+        let reg = /^(1\d{10}$/;
         try{
             if(!reg.test(value)) return 'match fail';
         }catch(e){
