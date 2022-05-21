e29cea7d684c0ebbcef8f06754c819f79f2cbb97
bigC <814151789@qq.com>
2022-05-21 17:34:47 +0800

    update code

--- app/io/controller/defalut.js
+++ app/io/controller/defalut.js
@@ -4,7 +4,11 @@ class DefaultController extends Controller {
     async index(){
         const { ctx,app } = this;
         const message = ctx.args[0];
-        app.io.of('/').emit('res', message);
+        const msg = {
+            ...message,
+            date:new Date()
+        }
+        app.io.of('/').emit('res', msg);
     }
 }
 
