3be81bc5a2d8047c1ce269d567047e44415e43de
bigC <814151789@qq.com>
2022-05-24 21:35:54 +0800

    update code

--- app/controller/task.js
+++ app/controller/task.js
@@ -16,11 +16,18 @@ class TaskController extends BaseController {
 
     async release() {
         let { userId, taskId } = this.ctx.request.body;
-        let params = [];
+        let promises = [];
         userId.forEach(userId => {
-            params.push(this.ctx.service.mysql.create("task_record", { userId, taskId }));
+            let sql = `insert into ${this.tablename} 
+            set 
+                user_id = ${userId} task_id = ${taskId} 
+            where 
+                (select count(*) from ${this.tablename} where user_id = ${userId} and task_id = ${taskId} = 0)
+            `;
+            let promise = this.ctx.service.mysql.query(sql);
+            promises.push(promise);
         });
-        await Promise.all(params);
+        await Promise.all(promises);
         return this.ctx.success({
             data: [],
             status: 1,
