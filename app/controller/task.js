866b353c5b937ca5e172d9f6a01380bdac490e17
bigC <814151789@qq.com>
2022-05-25 15:09:28 +0800

    update code

--- app/controller/task.js
+++ app/controller/task.js
@@ -8,17 +8,17 @@ class TaskController extends BaseController {
         this.tablename = 'task';
     }
 
-    async list(){
+    async list() {
         let mysql = this.ctx.service.mysql;
-        return await mysql.pagination(({limit, offset})=>{
-            let sql = `select 
-            task.id as id, task.name as task_name,task.desc as 'desc', task.user_id as user_id, 
+        return await mysql.pagination(({ limit, offset }) => {
+            let sql = `select task.id as id, task.name as task_name,task.desc as 'desc', task.user_id as user_id, 
             task.duration as duration, task.created_at as created_at, 
             task.updated_at as updated_at, task.level as level, user.name as user_name from task
             inner join user_info as user
             on user.id = task.user_id
-            limit ${ limit }
-            offset ${ offset }
+            order by task.level  desc
+            limit ${limit}
+            offset ${offset}
             `
             let count = mysql.count(this.tablename);
             let list = mysql.query(sql);
