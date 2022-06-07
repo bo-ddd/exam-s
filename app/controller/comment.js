fe9d88f164ff7b1be0a6c3447f7d012cf750e40e
SYuH <834725611@qq.com>
2022-06-07 08:24:15 +0800

    comment

--- app/controller/comment.js
+++ app/controller/comment.js
@@ -7,17 +7,18 @@ class CommentController extends BaseController {
         super(ctx);
         this.tablename = 'comment';
     }
+
     async list() {
         let mysql = this.ctx.service.mysql;
         return await mysql.pagination(({ limit, offset }) => {
             let sql = `
             select 
             comment.id as comment_id,comment_content,comment.created_at as comment_created_at,
-            user.username,user.id as user_id,
+            user_info.name as username,user_info.id as user_id,
             task.name as task_name,task.id as task_id
             from comment 
             inner join task on comment.task_id=task.id
-            inner join user on comment.user_id=user.id
+            inner join user_info on comment.user_id=user_info.id
             limit ${limit}
             offset ${offset}
             `
