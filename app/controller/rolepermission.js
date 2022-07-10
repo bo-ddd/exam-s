72262720f84a114cb50c5be2f94288bea4404e6e
shangyuhao <834725611@qq.com>
2022-07-10 17:30:31 +0800

    update rolepermission

--- app/controller/rolepermission.js
+++ app/controller/rolepermission.js
@@ -5,43 +5,40 @@ const BaseController = require('./base.js');
 class RolePermissionController extends BaseController {
     constructor(ctx) {
         super(ctx);
-        this.tablename = 'role_permission';
+        this.tablename = 'jurisdiction';
     }
 
     async create() {
         const { ctx } = this;
-        let { title } = ctx.request.body;
+        let { permissionId, roleId } = ctx.request.body;
         let promises = [];
-        title.forEach(title => {
-           let res =  ctx.service.mysql.create(this.tablename, {title});
-           promises.push(res);
+        permissionId.forEach(permissionId => {
+            let res = ctx.service.mysql.create(this.tablename, { permissionId, roleId });
+            promises.push(res);
         });
-       let res =  await Promise.all(promises);
-       return res.length === title.length ? ctx.success() : ctx.fail();
-      }
+        let res = await Promise.all(promises);
+        return res.length === title.length ? ctx.success() : ctx.fail();
+    }
 
     async list() {
-        let {ctx} = this;
-        // let sql = `select id from user where user_id = ${ctx.session.user.id}`;
-        // let data = await ctx.service.mysql.query(sql);
-        // let sqlrole = `select id from role where user_id = ${data[0].id}`;
-        // let id = await ctx.service.mysql.query(sqlrole);
-        // if(!id) ctx.fail({msg:"该用户下面还没有创建角色"});
-        // let mysql = ctx.service.mysql;
-        // return await mysql.pagination(({ limit, offset }) => {
-        //     let sql = `select  role.id as role_id,role_name,title
-        //     from role 
-        //     inner join role_permission
-        //     on role.id = role_permission.r_id
-        //     where r_id = ${id[0].id}
-        //     limit ${limit}
-        //     offset ${offset}
-        //     `
-        //     let count = mysql.count(this.tablename);
-        //     let list = mysql.query(sql);
-        //     return [count, list]
-        // })
-        return ctx.service.mysql.list(this.tablename,{pagination:false});
+        let { ctx } = this;
+        let { roleId } = ctx.request.body;
+        let mysql = ctx.service.mysql;
+        return await mysql.pagination(({ limit, offset }) => {
+            let sql = `    
+            select jurisdiction.id as id,role_name,role_permission.title from jurisdiction 
+            inner join role_permission
+            on jurisdiction.permission_id = role_permission.id
+            inner join role
+            on jurisdiction.role_id = role.id
+            where role.id = ${roleId}
+            limit ${limit}
+            offset ${offset}
+            `
+            let count = mysql.count(this.tablename);
+            let list = mysql.query(sql);
+            return [count, list]
+        })
     }
 }
 
